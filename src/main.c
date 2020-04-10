ebf3b20ee3c627a4c08baf7d8a02e505a2e56dd3
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-04-09 19:03:02 -0700

    clean redundant function, and add some files

--- src/main.c
+++ src/main.c
@@ -9,9 +9,6 @@ void main()
     unsigned long int el;
     //get command
     int HELLO = 0;
-    int HELP = 0;
-    int REBOOT = 0;
-    int TIMESTAMP = 0;
     //declare done
 
     // set up serial console
@@ -36,32 +33,10 @@ void main()
 		
         //which command
         HELLO = StrCmp(input, "hello", length, 5);
-        HELP = StrCmp(input, "help", length, 4);
-        REBOOT  = StrCmp(input, "reboot", length, 6);
-        TIMESTAMP = StrCmp(input, "timestamp", length, 9);
         //command detection done
 
 			
-        unsigned long long int time_counter, time_fre;
-			
-        asm volatile("mrs %0, CNTPCT_EL0" : "=r" (time_counter));
-        asm volatile("mrs %0, CNTFRQ_EL0" : "=r" (time_fre));
         if(HELLO == 1) uart_puts("\rHello World!\n");
-        else if(HELP) uart_puts("\rhello : print Hello World!\nhelp : help\nreboot : reboot rpi3\ntimestamp : get current timestamp\n");
-        else if(REBOOT)
-        {
-            uart_puts("\n\r");
-            reset(1000); 
-        }
-        else if(TIMESTAMP)
-        {
-            //print the timestamp
-            uart_puts("\r[ ");
-            Print_Int(time_counter);
-            uart_puts(" / ");
-            Print_Int(time_fre);
-            uart_puts(" ]\n");
-        }
         else if(length != 0)
         { 
             uart_puts("\rcommand  ");
@@ -76,8 +51,5 @@ void main()
         for(i=0;i<100;i++) input[i] = '0';
         length = 0;
         HELLO = 0;
-        HELP = 0;
-        REBOOT = 0;
-        TIMESTAMP = 0;
     }
 }
